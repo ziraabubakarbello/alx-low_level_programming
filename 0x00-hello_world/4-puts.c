cd774c783e0a502c61ffb5c71872358775c7c984
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-16 15:24:29 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Print a line of code using puts
+ *
+ * return:0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
