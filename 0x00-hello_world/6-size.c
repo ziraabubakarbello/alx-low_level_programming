5f5130796c59eb3e19fff88391e870db76915b5f
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-16 16:50:00 -0800

    clean code for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - C program that prints the size of various types on the computer it is compiled and run on
+ * main - Print out sizes of data types
  *
  * Return: 0
 */
