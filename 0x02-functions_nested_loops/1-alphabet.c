6a6d07e1addc5051f5cc14bdbda21b9c96c70d2f
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-21 11:16:02 -0800

    printing alphabets in lower cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print alphabet in lower cases
+ *
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; a < 'z'; letter++)
+	{
+		_putchar(letter);
+		_putchar('\n');
+	}
+}
