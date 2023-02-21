e447f7364a2a047898f5691abf194f6228db435b
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-21 14:17:42 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that prints letters either upper or lower case
+ * @c: character that takes in interger
+ * Return: 1 if true or 0 if false
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a') && (c <= 'z') || (C >= 'A') && (C <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
