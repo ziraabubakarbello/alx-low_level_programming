3e34f0acb6b9205f129f5dd6773266ffc8247c89
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-22 03:41:45 -0800

    natutal numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - sum of multiple of integers
+ * Return: 0 for success
+*/
+
+int main(void)
+{
+	int i, result;
+	int final = 1024;
+
+	for (i = 0; i <= final; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			result += i;
+		}
+	}
+	printf("%d\n ", result);
+	return (0);
+}
