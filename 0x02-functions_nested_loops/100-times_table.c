98afabc33d2d151a09c0aa915568fda287edb215
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-22 04:52:47 -0800

    100 times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the times table for n.
+ * @n: argument taken
+ * Return: 0 success
+*/
+
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (result < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 10 && result < 100)
+				{
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 100 && j != 0)
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					_putchar((result % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
