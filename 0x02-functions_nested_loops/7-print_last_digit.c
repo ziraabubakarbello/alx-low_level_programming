bb9eaaa8198bbf484620c89352b69a77b46248f9
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-21 17:13:56 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digits of a number
+ * @n: the parameter passed
+ * Return: return the last digit
+*/
+
+int print_last_digit(int n)
+{
+	int n;
+
+	int result = n % 10;
+
+	return (result);
+}
