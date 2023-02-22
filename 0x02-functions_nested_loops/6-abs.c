2a5385172ce9ac161fa076db30958a9491a6bb5f
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-21 16:56:53 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - function that prints the absolute int
+ * @n: absolute integer
+ * Return: absolute value
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
