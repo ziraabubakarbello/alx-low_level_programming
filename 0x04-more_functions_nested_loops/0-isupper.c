99b36bfb5aab13cadaddae3a1c043d93a0fde7ba
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-02-24 13:51:05 -0800

    task 0 and 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+"include <stdio.h>
+/**
+*_isupper - function to print letters if upper case
+*@c: argument taking
+*Return: 1 or 0
+*/
+
+int _isupper(int c)
+{
+	if (c => 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to print digits
+ * @c: parameter taking
+ * Return: 1 or 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
