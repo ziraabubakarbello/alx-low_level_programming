9324906576137c02d4338f38be27739bffe9eaa0
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-03-01 10:44:49 -0800

    advance task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13- function to encode a string
+ * @s: parameter accepted
+ * Return: returns encoded strings
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char letter[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char scramble_letter[] = "AJjDeJkLmwoqWZkklHxaFghpqSdUvWPrTtRz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letter[j])
+			{
+				s[i] = scramble_letter[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
