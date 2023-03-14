8d7a5ac5ce92555bd27f522010ab65e77cf56531
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-03-14 10:53:49 -0700

    100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,4 +6,7 @@ int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
 #endif
