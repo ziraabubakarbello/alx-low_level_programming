a6b6005a9e3e665ab4b5708152cce2450dc58ed0
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-03-17 03:51:57 -0700

    malloc creation

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat- concatinating two strings
+ * @s1: first parameter
+ * @s2: second parameter
+ * @n: third parameter
+ * Return: rreturn 0
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
