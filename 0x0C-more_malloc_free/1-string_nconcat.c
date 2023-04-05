8bafeb2f0cb3f6719fe0b96347da4210a1c7734e
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-04-05 13:32:16 -0700

    remarking

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,13 +1,12 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat- concatinating two strings
- * @s1: first parameter
- * @s2: second parameter
- * @n: third parameter
- * Return: rreturn 0
+* *string_nconcat - concatenates two strings
+* @s1: string to join to
+* @s2: string values to concatenate from
+* @n: number of bytes from s2 to concatenate to s1
+* Return: pointer to the resulting string
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -16,32 +15,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
+	{
 		len1++;
+	}
 	while (s2 && s2[len2])
+	{
 		len2++;
-
+	}
 	if (n < len2)
+	{
 		s = malloc(sizeof(char) * (len1 + n + 1));
+	}
 	else
+	{
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
+	}
 	if (!s)
+	{
 		return (NULL);
-
+	}
 	while (i < len1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
-
 	while (n < len2 && i < (len1 + n))
+	{
 		s[i++] = s2[j++];
-
+	}
 	while (n >= len2 && i < (len1 + len2))
+	{
 		s[i++] = s2[j++];
-
+	}
 	s[i] = '\0';
-
 	return (s);
 }
 
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
+* *_memset - fills memory with a constant byte
+* @s: memory area to be filled
+* @b: character value to copy
+* @n: number of times to copy variable b
+* Return: pointer to the memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -17,30 +15,29 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
-
 	return (s);
 }
-
 /**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- *
- * Return: pointer to allocated memory
- */
+* *_calloc - allocates memory for an array
+* @nmemb: number of elements in the array
+* @size: size of each elements in bytes
+* Return: pointer to allocated memory
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
-
+	}
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
+	{
 		return (NULL);
-
+	}
 	_memset(ptr, 0, nmemb * size);
-
 	return (ptr);
 }
+
